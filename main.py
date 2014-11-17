e95f6b6051262efe85d09893d8a4cc4394a56385
Rahul <rmanghwani@ebay.com>
2014-11-17 03:17:33 -0800

    Added time based variables. Current valid error = 0.61

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from sklearn.preprocessing import OneHotEncoder;
 import csv;
 import matplotlib.pyplot as plt;
 from sklearn.ensemble.partial_dependence import plot_partial_dependence;
+import time
 
 class IO(object):
     def __init__(self, s):
@@ -30,21 +31,21 @@ class IO(object):
 
 
 class Model(object):
-    def __init__(self, schema, input_data, mode):
+    def __init__(self, schema, input_data, mode, one_hot_encoding):
         self.schema = schema
         #Model param
-        self.model_param = {'n_estimators': 100, 'max_depth': 5, 'min_samples_split': 1, \
+        self.model_param = {'n_estimators': 1000, 'max_depth': 6, \
                             'learning_rate': 0.1, 'loss': 'ls', 'verbose' : 1}
-        if mode == 0:
-            #Train
-            self.data_label ,  self.data_features = self.preprocess(input_data, mode)
+        if mode in [0,2]:
+            #Train/validate
+            self.data_label ,  self.data_features = self.preprocess(input_data, mode, one_hot_encoding)
         else:
             #Test - We don't have the labels.
-            self.data_features = self.preprocess(input_data, mode)
+            self.data_features = self.preprocess(input_data, mode, one_hot_encoding)
 
-    def preprocess(self, data, mode):
+    def preprocess(self, data, mode, one_hot_encoding=None):
 
-        if mode == 0:
+        if mode in [0,2]:
           #Ensure label is not part of features
           feature_range = len(data[0]) - 1
           #Extract the label for train
@@ -61,9 +62,27 @@ class Model(object):
         for ele in data:
             dp = []
             for x in range(0, feature_range):
-                if x in [0,9,10]:
-                    #Need to transform datetime
+                if x in [9,10]:
                     pass
+                elif x == 0:
+                    parse_dt = time.strptime(ele[x], "%Y-%m-%d %H:%M:%S")
+                    #Create new features
+                    dp.append(int(parse_dt.tm_hour)) #Time of hour
+                    dp.append(int(parse_dt.tm_wday)) #Week day
+                    #dp.append(int(parse_dt.tm_mon)) #Month
+                    if int(parse_dt.tm_hour) >= 0 and int(parse_dt.tm_hour) < 6:
+                        dp.append(1)
+                    elif int(parse_dt.tm_hour) >= 6 and int(parse_dt.tm_hour) <= 9:
+                        dp.append(2)
+                    elif int(parse_dt.tm_hour) > 10 and int(parse_dt.tm_hour) <= 15:
+                        dp.append(3)
+                    elif int(parse_dt.tm_hour) > 16 and int(parse_dt.tm_hour) <= 19:
+                        dp.append(4)
+                    else:
+                        dp.append(5)
+                       
+                        
+                     
                 elif x in [1,2,3,4]:
                     #Categorical features
                     dp.append(int(ele[x]))
@@ -71,18 +90,36 @@ class Model(object):
                     #Numerical features
                     dp.append(float(ele[x]))
             features.append(dp)
-
+        
         #Update the schema accordingly
-        self.schema = ['season1', 'season2', 'season3', 'season4', 'holiday1', 'holiday2', 'weekend1', 'weekend2', \
+        self.schema = ['hr1', 'hr2', 'hr3', 'hr4', 'hr5', 'hr6', \
+                       'hr7', 'hr8', 'hr9', 'hr10', 'hr11', 'hr12', \
+                       'hr13', 'hr14', 'hr15', 'hr16', 'hr17', 'hr18', \
+                       'hr19', 'hr20', 'hr21', 'hr22', 'hr23', 'hr24', \
+                       'tw1', 'tw2', 'tw3', 'tw3', 'tw4', 'tw5', 'tw6', 'tw7', \
+                        # 'm1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'm11', 'm12', \
+                        'tb1', 'tb2', 'tb3', 'tb4', 'tb5', \
+                       'season1', 'season2', 'season3', 'season4', 'holiday1', 'holiday2', 'weekend1', 'weekend2', \
                        'weather1', 'weather2', 'weather3', 'weather4', 'temp', 'atemp', 'humidity', 'windspeed']
 
 
         #One-hot encoding on categorical features
-        params = {'categorical_features' : np.array([0,1,2,3])}
+        params = {'categorical_features' : np.array([0,1,2,3,4,5,6])}
         enc = OneHotEncoder(**params)
-        features = enc.fit_transform(features).toarray()
-
+        
+         
         if mode == 0 :
+            #In Train mode, fit and dump
+            enc_o = enc.fit(features)
+            pickle.dump(enc_o, open(one_hot_encoding, "wb"))
+        elif mode in [1,2] :
+            #Validate and Test work off the same encoding as train
+            enc_o = pickle.load( open(one_hot_encoding, "rb") )
+            
+        
+        features = enc_o.transform(features).toarray()
+        
+        if mode in [0,2] :
            return  label, features
         else:
            return features
@@ -97,8 +134,11 @@ class Model(object):
         print self.schema
 
         #PDP
-        fig, axs = plot_partial_dependence(model, self.data_features, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-
+        fig, axs = plot_partial_dependence(model, self.data_features, [12, 13, 14, 15])
+        
+        plt.subplots_adjust(top=0.9)
+        fig.savefig('pdp.png')
+        
         return  model
 
     def predict(self, model):
@@ -112,7 +152,7 @@ class Model(object):
     def evaluate(self, y_predict):
         rmsle = 0.0
         for i in range(0, len(y_predict)):
-            rmsle = math.pow((math.log(y_predict[i] + 1) - math.log(self.data_label[i] + 1)), 2)
+            rmsle += math.pow((math.log(y_predict[i] + 1) - math.log(self.data_label[i] + 1)), 2)
         rmsle = math.sqrt( (rmsle / float(len(y_predict))) )
         print "Root Mean Squared Log Error " +  str(rmsle);
 
@@ -123,11 +163,12 @@ class Model(object):
 if __name__ == '__main__':
     args = list(sys.argv[1:])
     if len(args) < 2:
-        print "For Training -> python main.py 0 <train-file> <model_object>"
-        print "For Test     -> python main.py 1 <test-file> <model_object> <output-file>"
+        print "For Training -> python main.py 0 <train-file> <model_object> <one-hot-encoding>"
+        print "For validation -> python main.py 2 <validate-file> <model_object> <one-hot-encoding>"
+        print "For Test     -> python main.py 1 <test-file> <model_object> <one-hot-encoding> <output-file>"
         sys.exit(1)
 
-    if int(args[0]) in [0,1]:
+    if int(args[0]) in [0,1,2]:
         #Read the file
         file_handler     = args[1];
         #Read
@@ -135,24 +176,27 @@ if __name__ == '__main__':
           schema, content = IO(f.read()).parse();
 
         #Build a model object
-        m = Model(schema, content, int(args[0]))
+        m = Model(schema, content, int(args[0]), args[3])
         if int(args[0]) == 0 :
           model_obj = m.train()
           y_predict = m.predict(model_obj)
+          print "Training ... "
           m.evaluate(y_predict)
           #Save the object"
-          pickle.dump(model_obj, open(args[2], "wb"))
+          pickle.dump(model_obj, open(args[2], "wb"))      
         else:
           #Load the Model
           model_obj = pickle.load( open(args[2], "rb") )
           y_predict = m.predict(model_obj)
-          output = open(args[3], 'w')
-          output.write("datetime,count\n")
-          for i in range(0, len(y_predict)):
+          if int(args[0]) == 2:
+            print "Validation ..."
+            m.evaluate(y_predict)
+          else:    
+            output = open(args[4], 'w')
+            output.write("datetime,count\n")
+            for i in range(0, len(y_predict)):
               output.write(str(content[i][0]) +"," + str(y_predict[i]) + "\n")
-          output.close()
-
-
+            output.close()
 
     else:
         print "Invalid Input"
